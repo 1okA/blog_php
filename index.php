edc59db6dc5f9f852ebe6288de563f2872bf2bce
1oka <loktionov89.a@mail.ru>
2021-03-13 11:28:43 +0400

    add twig and shablons

--- index.php
+++ index.php
@@ -3,18 +3,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+//Create App
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view){
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Anton'
+    ]);
+    $response->getBody()->write("$body");
     return $response;
 });
 
